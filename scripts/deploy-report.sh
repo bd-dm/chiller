b0bf0a275494ff3e77d8101d7d76659510438021
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-11-29 18:46:23 +0300

    ci: Report deployment fix

--- scripts/deploy-report.sh
+++ scripts/deploy-report.sh
@@ -2,7 +2,9 @@ BRANCH=$(git rev-parse --abbrev-ref HEAD)
 REPORT_PATH="pull-requests/${BRANCH}/test-report"
 URL="https://bd-dm.github.io/chiller/${REPORT_PATH}"
 
-git checkout deployments
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
+git checkout origin deployments
 mkdir -p "${REPORT_PATH}"
 cp -a playwright-report/. "${REPORT_PATH}"
 git add .
