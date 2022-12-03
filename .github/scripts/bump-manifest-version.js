7b8296e0d2691f644a0def92682cc7b8eeac231b
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-12-03 12:36:14 +0300

    ci: fix

--- .github/scripts/bump-manifest-version.js
+++ .github/scripts/bump-manifest-version.js
@@ -2,7 +2,7 @@ const path = require("node:path");
 const fs = require("node:fs");
 
 const [_, __, version] = process.argv;
-const MANIFEST_PATH = path.resolve(__dirname, "../public/manifest.json");
+const MANIFEST_PATH = path.resolve(__dirname, "../../public/manifest.json");
 
 const manifest = JSON.parse(fs.readFileSync(MANIFEST_PATH).toString());
 manifest.version = version
