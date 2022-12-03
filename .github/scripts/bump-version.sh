1448345b7a2c5d28139e2c151f62355fae908fce
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-12-03 12:47:01 +0300

    ci: fix

--- .github/scripts/bump-version.sh
+++ .github/scripts/bump-version.sh
@@ -5,12 +5,16 @@ git config --global user.email "action@github.com"
 git config --global user.name "GitHub Action"
 git checkout master
 
-npm version patch
+npm version patch --git-tag-version false
 
 VERSION=$(npm pkg get version | cut -d "\"" -f 2)
 
 node "./.github/scripts/bump-manifest-version.js" "${VERSION}"
 
 git add ./public/manifest.json
+
 git commit -m "${VERSION}"
-git push -u origin master
+git tag "v${VERSION}"
+
+git push origin master
+git push origin "v${VERSION}"
