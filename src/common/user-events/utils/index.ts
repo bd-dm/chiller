cb27becf65ad9ce53160a65962e243942d89e2d3
Dmitry Bardyshev <43277344+bd-dm@users.noreply.github.com>
2022-12-05 22:02:12 +0300

    feat: Scrolling to element if it is not visible (#66)

--- src/common/user-events/utils/index.ts
+++ src/common/user-events/utils/index.ts
@@ -2,3 +2,4 @@ export * from "./get-action-param-value";
 export * from "./get-ctrl-or-cmd-modifier";
 export * from "./get-target-element";
 export * from "./pick-random-from-array";
+export * from "./scroll-to-element-if-not-visible";
