eecbb9d3a4074e2a0814e5ae4bff79dac9d37a91
Dmitry Bardyshev <brd.dmitry@gmail.com>
2022-12-05 21:59:15 +0300

    feat: Made overlay draggable

--- src/content/overlay-window/overlay-window.tsx
+++ src/content/overlay-window/overlay-window.tsx
@@ -1,12 +1,29 @@
-import { Component } from "solid-js";
+import Draggabilly from "draggabilly";
+import { Component, createSignal, onMount } from "solid-js";
 
 import { ScriptsList } from "./components";
 import styles from "./index.module.scss";
 
 const OverlayWindow: Component = () => {
+	const [ref, setRef] = createSignal<HTMLDivElement>();
+
+	onMount(() => {
+		const element = ref();
+		if (!element) {
+			return;
+		}
+
+		new Draggabilly(element, { handle: `.${styles.header}` });
+	});
+
 	return (
-		<div classList={{ [styles.overlay]: true, chiller: true }}>
-			<ScriptsList />
+		<div class={styles.positioner}>
+			<div ref={setRef} classList={{ [styles.overlay]: true, chiller: true }}>
+				<h3 class={styles.header}>Chiller scripts</h3>
+				<div class={styles.content}>
+					<ScriptsList />
+				</div>
+			</div>
 		</div>
 	);
 };
